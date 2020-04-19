ddae7d58979735e4ccec8afca51b7071d934d316
ardnahcivar <ravbhanage@gmail.com>
2020-04-19 12:55:09 +0530

    search added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import styles from './header.module.css'
+import styles from './header.module.css'    
+import { MdAccountCircle } from 'react-icons/md'
+import { MdSearch } from 'react-icons/md'
 
 const Header = props => {
     return (
         <header className={styles.header}>
+            <MdAccountCircle size={30} />
             <h4 className={styles.headerTitle}>Contacts</h4>
             <div className={styles.searchbox}>
+                <span className={styles.searchIcon}>
+                    <MdSearch size={25} color={'#a13acb'}  />
+                </span>
                 <input placeholder='Search'></input>
             </div>
         </header>
